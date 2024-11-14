1e7b5213e8f1c526e310ad1f136be996f4f276c1
姚PD <13558507+yao-shipeng@user.noreply.gitee.com>
2024-11-14 04:29:52 +0000

    update serve/app/controller/Sell.php.
    Signed-off-by: 姚PD <13558507+yao-shipeng@user.noreply.gitee.com>

--- serve/app/controller/Sell.php
+++ serve/app/controller/Sell.php
@@ -30,8 +30,13 @@ class Sell extends Acl{
             }
             $sql=frameScope($sql);//组织数据
             $sql=sqlAuth('sell',$sql);//数据鉴权
+            
+            //排序
+            $column=empty($input['column'])?'id':$input['column'];
+            $order=empty($input['order'])?'desc':$input['order'];
+            
             $count = Sells::where($sql)->count();//获取总条数
-            $info = Sells::with(['frameData','customerData','peopleData','userData','billData','costData','invoiceData','recordData'])->where($sql)->append(['extension'])->page($input['page'],$input['limit'])->order(['id'=>'desc'])->select()->toArray();//查询分页数据
+            $info = Sells::with(['frameData','customerData','peopleData','userData','billData','costData','invoiceData','recordData'])->where($sql)->append(['extension'])->page($input['page'],$input['limit'])->order([$column=>$order])->select()->toArray();//查询分页数据
             //关联单据
             if(!empty($info)){
                 $sor=Db::name('sor')->where([['id','in',array_column($info,'source')]])->select()->toArray();
